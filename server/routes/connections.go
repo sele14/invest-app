e51b889dc1cfaa38dac27b66f4cea7b26bd17fd7
sele14 <eirik.selander@gmail.com>
2022-01-18 21:33:14 +0000

    added connections

--- server/routes/connections.go
+++ server/routes/connections.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -11,26 +12,38 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func DBConnect() *mongo.Client {
+func DBClient() *mongo.Client {
 	// load content of env
 	err := godotenv.Load(".env")
 
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
 	
 	// get mongodb endpoint
 	mdbEndpoint := os.Getenv("MONGODB_URL")
-
+	
 	clientOptions := options.Client().
-		ApplyURI(mdbEndpoint)
-		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ApplyURI(mdbEndpoint)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return client
+	fmt.Println("Successfully connected to MongoDB")
 
+	return client
+}
+
+// client DB instance
+var Client *mongo.Client = DBClient()
+
+func connectCollection(client *mongo.Client, collectionName string) *mongo.Collection {	
+
+	var collection *mongo.Collection = client.Database("cluster0").Collection(collectionName)
+	
+	return collection
 }
