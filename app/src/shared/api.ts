6bc3b3d58e26267035115d31f588ded4966fdd7d
sele14 <eirik.selander@gmail.com>
2022-01-24 00:00:15 +0000

    ui to api start

--- app/src/shared/api.ts
+++ app/src/shared/api.ts
@@ -1,11 +1,21 @@
 import axios, { AxiosInstance } from "axios";
 
+interface ResponseType {
+  hey: string;
+}
+
 class Api {
   private static axiosInstance: AxiosInstance;
+
   static init() {
     this.axiosInstance = axios.create({
       // Endpoint URL
       baseURL: "http://localhost:5000",
     });
   }
+
+  static async get<ResponseType>(url: string) {
+    return await Api.axiosInstance.get(url);
+  }
+  static async post<ResponseType, DataType>(url: string, data?: DataType) {}
 }
